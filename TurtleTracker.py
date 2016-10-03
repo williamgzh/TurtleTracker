469b2332520fdc69769a68b0e48d99f1773e9b5b
williamgzh <gaozhihao@outlook.com>
2016-10-03 12:24:54 -0400

    Adds while loop to read all lines

--- TurtleTracker.py
+++ TurtleTracker.py
@@ -23,3 +23,82 @@ obsLon = lineData[6]                # Observation Longitude
 # Print information to the user
 print "According to record " + recordID, 
 print "Sara was seen at " + obsLat + " d LAT; " + obsLat + " d LON on " + obsDate
+
+
+
+# Task2.py
+#
+# Description: Reads in ARGOS data file and parses a line of ARGOS tracking data 
+#
+# Created by: John Fay (jpfay@duke.edu)
+# Created on: Oct 2012
+
+# Create a variable pointing to the file with no header
+fileName = "V:\\Scripting3\\SaraNoHeader.txt"
+
+# Open the file as a read-only file object
+fileObj = open(fileName, 'r')
+
+# Read the first line from the open file object
+lineString = fileObj.readline()
+
+# Close the file object
+fileObj.close()
+
+# Use the split command to parse the items in lineString into a list object
+lineData = lineString.split("\t")
+
+# Assign variables to specfic items in the list
+recordID = lineData[0]              # ARGOS tracking record ID
+obsDateTime = lineData[2]           # Observation date and time (combined)
+obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
+obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
+obsLC = lineData[3]                 # Observation Location Class
+obsLat = lineData[5]                # Observation Latitude
+obsLon = lineData[6]                # Observation Longitude
+
+# Print information to the user
+print "According to record " + recordID, 
+print "Sara was seen at " + obsLat + " d LAT; " + obsLat + " d LON on " + obsDate
+
+
+# Task3.py
+#
+# Description: Reads in ARGOS data and displays information to the user
+#
+# Created by: John Fay (jpfay@duke.edu)
+# Created on: Oct 2012
+
+# Create a variable pointing to the file with no header
+fileName = "V:\\Scripting3\\SaraNoHeader.txt"
+
+# Open the file as a read-only file object
+fileObj = open(fileName, 'r')
+
+# Read the first line from the open file object
+lineString = fileObj.readline()
+
+# Use a while loop to read each line, one at a time, until the end of the file is reached
+while lineString:
+
+    # Use the split command to parse the items in lineString into a list object
+    lineData = lineString.split("\t")
+
+    # Assign variables to specfic items in the list
+    recordID = lineData[0]              # ARGOS tracking record ID
+    obsDateTime = lineData[2]           # Observation date and time (combined)
+    obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
+    obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
+    obsLC = lineData[3]                 # Observation Location Class
+    obsLat = lineData[5]                # Observation Latitude
+    obsLon = lineData[6]                # Observation Longitude
+
+    # Print information to the user
+    print "According to record " + recordID, 
+    print "Sara was seen at " + obsLat + " d LAT; " + obsLat + " d LON on " + obsDate
+
+    # Read in the next line
+    lineString = fileObj.readline()
+    
+# Close the file object
+fileObj.close()
