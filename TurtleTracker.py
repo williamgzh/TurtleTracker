c6a9cf1503d20217b249b79bd86df8b30928eb4c
williamgzh <gaozhihao@outlook.com>
2016-10-03 12:19:06 -0400

    Revert "Changes name in comments"
    This reverts commit 0ea0d6aec37b23361e05f1c9debd798bf261da8d.

--- TurtleTracker.py
+++ TurtleTracker.py
@@ -3,8 +3,7 @@
 # Description: Parses a line of ARGOS tracking data 
 #
 # Created by: John Fay (jpfay@duke.edu)
-# Modified by: William Gao (zg32@duke.edu)
-# Created on: Oct 2016
+# Created on: Oct 2012
 
 # Copy and paste a line of data as the startLine variable value
 lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
